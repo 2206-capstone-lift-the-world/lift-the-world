642d9716eaf0dd59a1c58b60459e16ac72b27b58
Kyle Parkinson <kparki1130@gmail.com>
2022-09-02 12:40:18 -0400

    committing for pull

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -90,6 +90,10 @@ const User = db.define("user", {
     type: Sequelize.BOOLEAN,
     defaultValue: false,
   },
+  hasReached12: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 });
 
 module.exports = User;
