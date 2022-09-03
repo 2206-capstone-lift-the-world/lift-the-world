3718976a8bd165c4b1658b69af62f15d34c92b57
Cherry Xu <ms.cherry.xu@gmail.com>
2022-09-02 22:52:33 -0400

    Started working on modal components

---
+++ client/components/Modal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Modal = () => {
+  return <div>Hi</div>;
+};
+
+export default Modal;
---
+++ client/components/ModalOnLeveling.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import ModalOnLeveling from "./ModalOnLeveling";
+
+const ModalOnLeveling = () => {
+  return <div>ModalOnLeveling</div>;
+};
+
+export default ModalOnLeveling;
